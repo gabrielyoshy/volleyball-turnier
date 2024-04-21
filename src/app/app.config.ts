8611bae83bc3dfacd8aa58cbd83324dea01259eb
gabriel <gabriel.pedrozopetrazzini@gmail.com>
2024-04-21 21:01:10 +0200

    add firebase conection

---
+++ src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { Keys } from '../../keys';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(Keys.fireBaseConfig))
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
+};
