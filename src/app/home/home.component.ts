63bbfa40deeed17d708184e613cf557cb5b8322d
Gabriel Pedrozo <gabriel.pedrozo@mt-itsolutions.com>
2024-07-01 10:41:16 +0200

    add qr code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [QRCodeModule, TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
